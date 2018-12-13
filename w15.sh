744d323c63f3377ae0f13c6330b6ca8bd87c6502
AndriyTybinka <andriy.tybinka@gmail.com>
2018-12-13 15:33:49 +0200

    Add files via upload

--- w15.sh
+++ w15.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+touch a.txt
+#cp ./a.txt /opt/
 cd /opt
 touch f1 f2 f3 f4 f5
 current_datetime=$(date)
 echo "Current date:" $current_datetime
 ls -al
+
+while true; do sleep 36; done
 
